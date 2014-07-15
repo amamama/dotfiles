2a82f26890708ca1a0fc1396568c357f4395e582
amamama <amtkrogzo@yahoo.co.jp>
2014-07-15 16:46:34 +0900

    bug fix

--- lndotfile.sh
+++ lndotfile.sh
@@ -2,6 +2,7 @@
 #dotfiles/lndotfiles.sh
 
 ln -sF $(pwd)"/".bashrc ~/.bashrc
-ln -sF $(pwd)"/".bash_aliases ~/.bash_aleases
+ln -sF $(pwd)"/".bash_aliases ~/.bash_aliases
 ln -sF $(pwd)"/".vimrc ~/.vimrc
+ln -sF $(pwd)"/".Xresources ~/.Xresources
 
