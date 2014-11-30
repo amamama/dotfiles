d720b4af0244d5352c5b6efea9f850eb2d567a12
amamama <amtkrogzo@yahoo.co.jp>
2014-11-30 21:41:03 +0900

    tiny fix

--- lndotfile.sh
+++ lndotfile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #dotfiles/lndotfiles.sh
 
-ln -sF $(pwd)"/".bashrc ~/.bashrc
-ln -sF $(pwd)"/".bash_aliases ~/.bash_aliases
-ln -sF $(pwd)"/".bash_funcs ~/.bash_funcs
-ln -sF $(pwd)"/".vimrc ~/.vimrc
-ln -sF $(pwd)"/".Xresources ~/.Xresources
+ln -s $(pwd)"/".bashrc ~/.bashrc
+ln -s $(pwd)"/".bash_aliases ~/.bash_aliases
+ln -s $(pwd)"/".bash_funcs ~/.bash_funcs
+ln -s $(pwd)"/".vimrc ~/.vimrc
+ln -s $(pwd)"/".Xresources ~/.Xresources
 
