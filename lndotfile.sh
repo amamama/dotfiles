c3a2409ee498d6f5263a74774ff0d4fd7a13f726
amamama <amtkrogzo@yahoo.co.jp>
2015-01-25 20:02:43 +0900

    add w3m files

--- lndotfile.sh
+++ lndotfile.sh
@@ -7,3 +7,5 @@ ln -s $(pwd)"/".bash_funcs ~/.bash_funcs
 ln -s $(pwd)"/".vimrc ~/.vimrc
 ln -s $(pwd)"/".Xresources ~/.Xresources
 
+ln -s $(pwd)"/".w3m/config ~/.w3m/config
+ln -s $(pwd)"/".w3m/keymap ~/.w3m/keymap
