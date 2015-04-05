64c2132191f0de398a6bc4b1b2dafbecb3ab6b34
amamama <amtkrogzo@yahoo.co.jp>
2015-04-05 18:42:20 +0900

    update

--- lndotfile.sh
+++ lndotfile.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #dotfiles/lndotfiles.sh
 
-ln -s $(pwd)"/".bashrc ~/.bashrc
-ln -s $(pwd)"/".bash_aliases ~/.bash_aliases
-ln -s $(pwd)"/".bash_funcs ~/.bash_funcs
-ln -s $(pwd)"/".vimrc ~/.vimrc
-ln -s $(pwd)"/".Xresources ~/.Xresources
+ln -s -i $(pwd)"/".bashrc ~/.bashrc
+ln -s -i $(pwd)"/".bash_aliases ~/.bash_aliases
+ln -s -i $(pwd)"/".bash_funcs ~/.bash_funcs
+ln -s -i $(pwd)"/".vimrc ~/.vimrc
+ln -s -i $(pwd)"/".Xresources ~/.Xresources
 
-ln -s $(pwd)"/".w3m/config ~/.w3m/config
-ln -s $(pwd)"/".w3m/keymap ~/.w3m/keymap
+ln -s -i $(pwd)"/".w3m/config ~/.w3m/config
+ln -s -i $(pwd)"/".w3m/keymap ~/.w3m/keymap
