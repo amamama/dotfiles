d16f715f6b9681914905a5eafd4593e18f4c48fa
amama <amama@ikiu.me>
2019-03-31 05:52:58 +0000

    update

--- lndotfiles.sh
+++ lndotfiles.sh
@@ -3,14 +3,14 @@
 
 lnfiles() {
 	for i in $(ls -A) ;do
-		if [ $(echo ${i}|grep "~") ]; then continue; fi
-		if [ $(echo ${i}|grep ".git") ]; then continue; fi
-		if [ $(echo ${i}|grep "lndotfiles") ]; then continue; fi
-		if [ $(echo ${i}|grep "req_pkg") ]; then continue; fi
-		if [ -f ${i} -a -d ~/$1 -a \( ! -e ~/$1/${i} -o ! -h ~/$1/${i} \) ]; then
+		if [[ $i =~ ~$ ]]; then continue; fi
+		if [[ $i =~ git ]]; then continue; fi
+		if [[ $i =~ lndotfiles ]]; then continue; fi
+		if [[ $i =~ req_pkg ]]; then continue; fi
+		if [[ -f ${i} && -d ~/$1 && !(( -e ~/$1/${i} && -h ~/$1/${i} )) ]]; then
 			echo "ln -s -i $(pwd)/${i} ~/$1/${i}"
 			ln -s -i $(pwd)/${i} ~/$1/${i}
-		elif [ -d ${i} -a -d ~/${i} ]; then
+		elif [[ -d ${i} && -d ~/${i} ]]; then
 			pushd ${i}
 			lnfiles $1/${i}
 			popd
